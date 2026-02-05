865d136fa40a33fa23f3467e7d4f1070856aa113
Serhii M DevOps Engineer <serhiimdevopsengineer@MacBook-Pro-Serhii.local>
2026-02-05 16:46:56 +0200

    add values

--- scripts/test_pipeline.groovy
+++ scripts/test_pipeline.groovy
@@ -21,7 +21,7 @@ spec:
         string(name: 'BRANCH', defaultValue: 'main', description: 'Git branch')
         string(name: 'CONFIG_REPO_URL', defaultValue: 'git@github.com:messsi10/configuration-repo.git', description: 'Config repo SSH URL (contains env values)')
         string(name: 'CONFIG_BRANCH', defaultValue: 'main', description: 'Config repo branch')
-        string(name: 'CONFIG_VALUES_FILE', defaultValue: 'configuration-repo/socketio-test-service/dev-values.yaml', description: 'Path to values file inside workspace')
+        choice(name: 'ENV', choices: ['dev', 'prod'], description: 'Deployment environment (selects values file)')
         string(name: 'NAMESPACE', defaultValue: 'socketio-namespace', description: 'Kubernetes namespace')
         string(name: 'RELEASE_NAME', defaultValue: 'socketio', description: 'Helm release name')
     }
@@ -53,7 +53,8 @@ spec:
         stage('Load values.yaml') {
         steps {
             script {
-            def v = readYaml file: params.CONFIG_VALUES_FILE
+            def valuesFile = "configuration-repo/socketio-test-service/${params.ENV}-values.yaml"
+            def v = readYaml file: valuesFile
             env.IMAGE_TAG_FROM_VALUES = (v?.image?.tag ?: '0.0.1').toString()
             env.IMAGE_REPO_FROM_VALUES = (v?.image?.repository ?: 'smeleshchyk/socketio-test').toString()
                    }
